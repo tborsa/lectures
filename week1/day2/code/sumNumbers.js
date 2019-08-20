3bb3c6b943c7fa4b1627c191afa72f47fd061a41
Travis  Borsa <travisborsa@Traviss-MacBook-Air.local>
2019-08-20 12:20:26 -0700

    after w1d2 lecture

---
+++ week1/day2/code/sumNumbers.js
@@ -0,0 +1,30 @@
+
+//Read arguments from command line
+//loop through these arguments to test
+const getArguments = function() {
+  const userArgs = process.argv.slice(2);
+  return userArgs;
+};
+
+//Check if input is a whole number
+//If its not a whole number skip it
+const checkWholesomeness = function(num) {
+  const isNumber = Number(num);
+  const isWhole = Number.isInteger(isNumber);
+  function myTest(){console.log("her")};
+  if (isNumber && isWhole) {
+    return isNumber;
+  }
+  return 0;
+};
+
+//Add the numbers
+const addWholeNumbers = function(numArr) {
+  let sum = 0;
+  for (let element of numArr) {
+    sum += checkWholesomeness(element);
+  }
+  return sum;
+};
+
+console.log(addWholeNumbers(getArguments()));
